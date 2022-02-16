18c6f2c2775499d568e324b5fd3da76832674376
Cedric Louyot <cedric.louyot@gmail.com>
2022-02-16 15:20:56 +0100

    Flat index

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Begin seeds.rb'
+puts 'Destroy all flats'
+Flat.destroy_all
+
+puts 'Generate new flats'
+
+4.times do
+  flat = Flat.create(
+    name: Faker::Commerce.product_name,
+    address: Faker::Address.street_address,
+    description: Faker::Lorem.paragraph,
+    price_per_night: (50..200).to_a.sample,
+    number_of_guests: (1..8).to_a.sample
+  )
+  puts "Generated flat #{flat.id}"
+end
+
+puts 'End of seed.rb'
